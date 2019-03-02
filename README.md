d5d10315543beae6db2083f16d00f85159cabd38
Nelson-Lemercier <nelson.lemercier@laposte.net>
2019-03-02 18:31:43 +0200

    Put the answers in the report, updated the scripts to do the revised tasks

--- README.md
+++ README.md
@@ -1,33 +1,18 @@
 # Mininet
 First exercise of the communication networks II course
 
-# TODO
+# Tasks
 ##Q1
 
-**a** Should add the tests in the script. To make the tests automatically:
-```
-sudo mn --test iperf
-``` 
-
-**d** Added a answers folder with some results of the performance tests
-
-[x]  5 Mb/s
-[]  10 Mb/s
-[]  100 Mb/s
+Updated scripts, all results in the report
 
 ##Q2
 
-It seems mininet need a plugin for the POX controller. It is available here :
-
-```
-https://github.com/noxrepo/pox
-```
-
-Need to check if the tables are correct.
+Added needed scripts in script/pox
 
 ##Q3
 
-Topology and pingall test done.
+Topology and pingall test done
 
 ##Q4
 
--- scripts/ex3_CNII.py
+++ scripts/ex3_CNII.py
@@ -1,34 +1,70 @@
-from mininet.topo import Topo
+#!/usr/bin/python
 
-class TopoQ3( Topo ):
-    "Simple topology example."
+"""
+This example creates a multi-controller network from semi-scratch by
+using the net.add*() API and manually starting the switches and controllers.
 
-    def __init__( self ):
-        "Create custom topo."
+This is the "mid-level" API, which is an alternative to the "high-level"
+Topo() API which supports parametrized topology classes.
 
-        # Initialize topology
-        Topo.__init__( self )
-
-        # Add hosts and switches
-        SwitchA = self.addSwitch( 's1' )
-        SwitchB = self.addSwitch( 's2' )
-        SwitchC = self.addSwitch( 's3' )
-        SwitchD = self.addSwitch( 's4' )
-        Host1 = self.addHost( 'Host1' )
-        Host2 = self.addHost( 'Host2' )
-        Host3 = self.addHost( 'Host3' )
-        Host4 = self.addHost( 'Host4' )
-
-        # Add links
-
-        self.addLink( Host1, SwitchA )
-        self.addLink( SwitchA, SwitchB )
-        self.addLink( SwitchB, SwitchC )
-        self.addLink( SwitchC, Host3 )
-        self.addLink( Host2, SwitchB )
-        self.addLink( Host4, SwitchC )
-        self.addLink( SwitchA, SwitchD )
-        self.addLink( SwitchD, SwitchB )
+Note that one could also create a custom switch class and pass it into
+the Mininet() constructor.
+"""
 
 
-topos = { 'topoq3': ( lambda: TopoQ3() ) }
+from mininet.net import Mininet
+from mininet.node import Controller, OVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+
+def multiControllerNet():
+    "Create a network from semi-scratch with multiple controllers."
+
+    net = Mininet( controller=Controller, switch=OVSSwitch )
+
+    info( "*** Creating (reference) controllers\n" )
+    c1 = net.addController( 'c1', port=6633 )
+
+    info( "*** Creating switches\n" )
+    s1 = net.addSwitch( 's1' )
+    s2 = net.addSwitch( 's2' )
+    s3 = net.addSwitch( 's3' )
+    s4 = net.addSwitch( 's4' )
+
+    info( "*** Creating hosts\n" )
+    
+    Host1 = net.addHost( 'Host1' )
+    Host2 = net.addHost( 'Host2' )
+    Host3 = net.addHost( 'Host3' )
+    Host4 = net.addHost( 'Host4' )
+
+    info( "*** Creating links\n" )
+    
+    net.addLink( Host1, s1 )
+    net.addLink( s1, s2 )
+    net.addLink( s2, s3 )
+    net.addLink( s3, Host3 )
+    net.addLink( Host2, s2 )
+    net.addLink( Host4, s3 )
+    net.addLink( s1, s4 )
+    net.addLink( s4, s2 )
+
+    info( "*** Starting network\n" )
+    net.build()
+    c1.start()
+    s2.start( [ c1 ] )
+    s3.start( [ c1 ] )
+    s4.start( [ c1 ] )
+
+    #info( "*** Testing network\n" )
+    #net.pingAll()
+
+    info( "*** Running CLI\n" )
+    CLI( net )
+
+    info( "*** Stopping network\n" )
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )  # for CLI output
+    multiControllerNet()
